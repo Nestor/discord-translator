0f49a57cd05b812ecd73444dfdbeffadb4f5b71f
aziz <azizgfx@gmail.com>
2017-12-11 08:12:30 +0200

    Added command to display list of supported languages

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,16 +1,40 @@
-/*eslint-disable*/
+const setStatus = require("../core/status");
+const botSend = require("../core/send");
+
+// --------------------
+// Supported Languages
+// --------------------
+
 module.exports = function(data)
 {
-/*
-   // --------------------
-   // Supported Languages
-   // --------------------
+   setStatus(data.bot, "startTyping", data.message.channel);
 
-   output = "\n";
+   const langList = [
+      "Afrikaans","Albanian","Amharic","Arabic","Armenian","Azerbaijani",
+      "Basque","Belarusian","Bengali", "Bosnian","Bulgarian","Catalan",
+      "Cebuano","Chichewa","Chinese Simplified","Chinese Traditional",
+      "Corsican","Croatian","Czech","Danish","Dutch","English","Esperanto",
+      "Estonian","Filipino","Finnish", "French","Frisian","Galician",
+      "Georgian","German","Greek","Gujarati","Haitian Creole","Hausa",
+      "Hawaiian", "Hebrew","Hindi","Hmong","Hungarian","Icelandic","Igbo",
+      "Indonesian","Irish","Italian","Japanese", "Javanese","Kannada",
+      "Kazakh","Khmer","Korean","Kurdish (Kurmanji)","Kyrgyz","Lao",
+      "Latin","Latvian","Lithuanian","Luxembourgish","Macedonian",
+      "Malagasy","Malay","Malayalam","Maltese","Maori","Marathi",
+      "Mongolian","Myanmar (Burmese)","Nepali","Norwegian","Pashto",
+      "Persian","Polish","Portuguese","Punjabi", "Romanian","Russian",
+      "Samoan","Scots Gaelic","Serbian","Sesotho","Shona","Sindhi",
+      "Sinhala","Slovak", "Slovenian","Somali","Spanish","Sundanese",
+      "Swahili","Swedish","Tajik","Tamil","Telugu","Thai","Turkish",
+      "Ukrainian","Urdu","Uzbek","Vietnamese","Welsh","Xhosa","Yiddish",
+      "Yoruba","Zulu"
+   ];
 
-   text.langList.forEach(function(lang)
+   var output = "**```Supported Languages```**\n";
+
+   langList.forEach(function(lang)
    {
-      var end = " ,  ";
+      var end = ",  ";
       if (lang === "Zulu")
       {
          end = ".";
@@ -18,11 +42,8 @@ module.exports = function(data)
       output += "`" + lang + "`" + end;
    });
 
-   sendBox({
-      channel: channel.id,
-      color: colorInfo,
-      text: text.supported + output
-   });
-   return;
-*/
+   data.color = "info";
+   data.text = output;
+
+   return botSend(data);
 };
--- src/i18n/en.js
+++ src/i18n/en.js
@@ -71,21 +71,6 @@ exports.errMaxMulti = function (max)
    return `:warning:  You can only set a maximum of ${max} languages for multi-translation.`;
 }
 
-exports.langList =  [
-   'Afrikaans','Albanian','Amharic','Arabic','Armenian','Azerbaijani','Basque','Belarusian','Bengali',
-   'Bosnian','Bulgarian','Catalan','Cebuano','Chichewa','Chinese Simplified','Chinese Traditional',
-   'Corsican','Croatian','Czech','Danish','Dutch','English','Esperanto','Estonian','Filipino','Finnish',
-   'French','Frisian','Galician','Georgian','German','Greek','Gujarati','Haitian Creole','Hausa','Hawaiian',
-   'Hebrew','Hindi','Hmong','Hungarian','Icelandic','Igbo','Indonesian','Irish','Italian','Japanese',
-   'Javanese','Kannada','Kazakh','Khmer','Korean','Kurdish (Kurmanji)','Kyrgyz','Lao','Latin','Latvian',
-   'Lithuanian','Luxembourgish','Macedonian','Malagasy','Malay','Malayalam','Maltese','Maori','Marathi',
-   'Mongolian','Myanmar (Burmese)','Nepali','Norwegian','Pashto','Persian','Polish','Portuguese','Punjabi',
-   'Romanian','Russian','Samoan','Scots Gaelic','Serbian','Sesotho','Shona','Sindhi','Sinhala','Slovak',
-   'Slovenian','Somali','Spanish','Sundanese','Swahili','Swedish','Tajik','Tamil','Telugu','Thai','Turkish',
-   'Ukrainian','Urdu','Uzbek','Vietnamese','Welsh','Xhosa','Yiddish','Yoruba','Zulu'
-];
-
-
 exports.helpMessage = function(version)
 {
    return "```md"
