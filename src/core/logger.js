ba555e55f779c80f21585e6499350a24bfecf5dd
aziz <azizgfx@gmail.com>
2017-12-19 01:46:56 +0200

    Logger for commands, errors and guild events

---
+++ src/core/logger.js
@@ -0,0 +1,128 @@
+const webhook = require("webhook-discord");
+const auth = require("./auth");
+const hook = new webhook(auth.loggerWebhook);
+const spacer = "​                                                          ​";
+
+// ------------
+// Command log
+// ------------
+
+const logCmd = function(data)
+{
+   var srv = "";
+   var chN = "dm";
+   var perm = "user";
+
+   if (data.message.channel.type === "text")
+   {
+      srv = `  in  \`${data.message.channel.guild.name}\``;
+      chN = `#${data.message.channel.name}`;
+
+      if (data.message.author.id === data.message.channel.guild.owner.id)
+      {
+         perm = "server owner";
+      }
+   }
+
+   if (data.message.isManager)
+   {
+      perm = "manager";
+   }
+   if (data.message.isAdmin)
+   {
+      perm = "admin";
+   }
+   if (data.message.author.id === data.config.owner)
+   {
+      perm = "bot owner";
+   }
+
+   hook.custom(
+      "Bot Logger",
+      "```py\n" +
+      `@${data.message.author.username}#${data.message.author.discriminator}` +
+      `\n${data.message.content}` +
+      "\n```" +
+      `\nvia  \`${chN}\`` + srv + spacer + spacer,
+      `Cmd by ${perm}`,
+      "#659fbe"
+   );
+};
+
+// ------------
+// Command log
+// ------------
+
+const logError = function(err)
+{
+   hook.custom(
+      "Bot Logger",
+      "```prolog\n" + err + "\n```",
+      `Error log`,
+      "#ff4747"
+   );
+};
+
+// ---------------
+// Guild Join Log
+// ---------------
+
+const logJoin = function(guild)
+{
+   hook.custom(
+      "Bot Logger",
+      `:white_check_mark:  **${guild.name}**\n` +
+      "```md\n> " + guild.id + "\n@" + guild.owner.user.username + "#" +
+      guild.owner.user.discriminator + "\n```" + spacer + spacer,
+      "Joined Guild",
+      "#65be8d"
+   );
+};
+
+// ----------------
+// Guild Leave Log
+// ----------------
+
+const logLeave = function(guild)
+{
+   hook.custom(
+      "Bot Logger",
+      `:regional_indicator_x:  **${guild.name}**\n` +
+      "```md\n> " + guild.id + "\n@" + guild.owner.user.username + "#" +
+      guild.owner.user.discriminator + "\n```" + spacer + spacer,
+      "Left Guild",
+      "#be7865"
+   );
+};
+
+// ====================
+// Analyze log request
+// ====================
+
+module.exports = function(type, data)
+{
+   // ------------------------------------------------
+   // Stop proccessing if hook URL is invalid/not set
+   // ------------------------------------------------
+
+   if (!hook || !hook.url || hook.url === "" || hook.url === " ")
+   {
+      return;
+   }
+
+   // ---------------
+   // Logging Events
+   // ---------------
+
+   const logEvents = {
+      "cmd": logCmd,
+      "error": logError,
+      "guildJoin": logJoin,
+      "guildLeave": logLeave
+   };
+
+   if (logEvents.hasOwnProperty(type))
+   {
+      return logEvents[type](data);
+   }
+};
