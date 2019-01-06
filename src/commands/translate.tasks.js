6e8e1a1543cbce1aaa11e9f8094e4a767dffcb01
Karl Grindley <karl@grindleyfamily.com>
2019-01-05 19:41:49 -0500

    debug

--- src/commands/translate.tasks.js
+++ src/commands/translate.tasks.js
@@ -122,11 +122,13 @@ console.log("shutTasks() - 1");
 console.log("shutTasks() - 2");
    for(var i = 0, len = res.length; i < len; i++) {
 	console.log("\n\ntask #" + i);
-	console.log(res[i]);
+	console.log(res[i].tasks);
 	console.log("\n\n\n");
-   	data.text = `:negative_squared_cross_mark:  : Translating ${res[i].tasks.dataValues.lang_from} messages from channel ${res[i].tasks.dataValues.origin} ` +
-		   `to ${res[i].tasks.dataValues.lang_to} messages on channel ${res[i].tasks.dataValues.dest}`
+	   /*
+   	data.text = `:negative_squared_cross_mark:  : Translating ${res[i].tasks.dataValues.lang_from} messages from **${res[i].tasks.dataValues.origin}** ` +
+		   `to ${res[i].tasks.dataValues.lang_to} messages to **${res[i].tasks.dataValues.dest}**`
 	botSend(data);
+	*/
    }
 
 console.log("shutTasks() - 3");
